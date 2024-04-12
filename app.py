1dd88bcae5aded1e3bcf3e5cc4c288e6ad015e87
nugosvianadze <nugzar.svianadze.1@btu.edu.ge>
2024-04-12 20:38:11 +0400

    initial commit

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+def middle(value):
+    if len(value) % 2 == 1:
+        len_value = (len(value) - 1) / 2
+        value = value[int(len_value)]
+    return value
+
+
+print(app.jinja_env.filters)
+app.jinja_env.filters['middle'] = middle
+
 @app.route('/')
 @app.route('/home')
 def home():
-    first_name, last_name = 'nugo', 'svianadze'
+    first_name, last_name = 'nugzari', 'svianadze'
+    num = 25
+    d = {'name': 'nugzari', 'age': 20}
     return render_template('home.html', first_name=first_name,
-                           last_name=last_name)
+                           last_name=last_name, num=num, d=d)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5100)
